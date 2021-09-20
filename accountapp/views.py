64a11be8670d4935dcaf5844f566e36f87d1a8ac
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 13:07:57 +0900

    for 문이용해서 binding render수정

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -12,10 +13,17 @@ def hello_world(request):
 
         # helloworld의 새로운객체가 여기로 들어감
         new_hello_world = HelloWorld()
-        # inp
+        # temp의 내용을 helloworld.text에 저장
         new_hello_world.text = temp
         # db에 데이터를 저장
         new_hello_world.save()
-        return render(request, 'acoountapp/hello_world.html', context={'hello_world_output':new_hello_world})# context = data꾸러미
+        # hello의 모든데이터ㅗ를 모두 긁어옴
+
+        hello_world_list=HelloWorld.objects.all()
+        # 반복출력 이유는..
+        # return render(request, 'acoountapp/hello_world.html', context={'hello_world_list':hello_world_list})# context = data꾸러미
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) #account앱에 helloworld 로 reverse하셈
     else:
-        return render(request, 'acoountapp/hello_world.html', context={'text':'GET METHOD!!!'})# context = data꾸러미
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'acoountapp/hello_world.html', context={'hello_world_list':hello_world_list})# context = data꾸러미
