54ef2bac14490dbb7172afff81a15089468fbbab
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 19:37:03 +0900

    form 유효성 체크

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    # form.py 에서 보낸 데이터가 여기의 param으로 들어옴
+    def form_valid(self, form):
+        #실 디비에 저장하지는 않고 임시저장중인상태
+        temp_profile = form.save(commit=False)
+        # 요청보낸 당사자의 user의 데이터를 temp_profile.user로 넘김
+        temp_profile.user = self.request.user
+        temp_profile.save()
+        return super().form_valid(form)
