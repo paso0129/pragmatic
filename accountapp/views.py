aede48f9ad29d3a8f2aa3ea6634a79004f1f77c2
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 12:56:42 +0900

    hello text sqlite에 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'acoountapp/hello_world.html', context={'text':'POST METHOD!!!'})# context = data꾸러미
+        #helloworld input 가져와
+        temp = request.POST.get('hello_world_input')
+
+        # helloworld의 새로운객체가 여기로 들어감
+        new_hello_world = HelloWorld()
+        # inp
+        new_hello_world.text = temp
+        # db에 데이터를 저장
+        new_hello_world.save()
+        return render(request, 'acoountapp/hello_world.html', context={'hello_world_output':new_hello_world})# context = data꾸러미
     else:
         return render(request, 'acoountapp/hello_world.html', context={'text':'GET METHOD!!!'})# context = data꾸러미
