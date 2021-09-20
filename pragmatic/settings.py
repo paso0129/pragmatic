9d6a4ae3eefe60bfe63e37194f599aca08368e58
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 21:14:08 +0900

    article crud 생성

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accountapp',
     'bootstrap4',
-    'profileapp'
+    'profileapp',
+    'articleapp',
 ]
 
 MIDDLEWARE = [
--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
     # root setting의 경로 추가 방법
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
