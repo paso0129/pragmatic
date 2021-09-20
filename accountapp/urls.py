5c66f2bd58e01f37615644be3ddfe8df374c1931
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 17:20:55 +0900

    login & logout기능 추가

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -5,7 +6,12 @@ from accountapp.views import hello_world, AccountCreateView
 app_name = "accountapp"
 
 urlpatterns = [
-    #함수형뷰와 CBV의 차이
+    #함수형뷰와 CBV의 차이 as_view사용
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     path('create/', AccountCreateView.as_view(), name='create'),
+
 ]
