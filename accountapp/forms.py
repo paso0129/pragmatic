abd276e8114c89ea10f49c820452fee2cd733aca
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 18:09:44 +0900

    username수정 불가 view 작성

---
+++ accountapp/forms.py
@@ -0,0 +1,9 @@
+# 수정시 id 못바꾸게
+
+from django.contrib.auth.forms import UserCreationForm
+
+class AccountUpdateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].disabled = True
--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 # 장고에서 제공하는 cbv의 제너릭뷰
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
@@ -50,3 +51,11 @@ class AccountDetailView(DetailView):
     # template에서 사용하는 유저 객체의 이름
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    #성공햇다면 어디로 보낼것인가
+    # 클래스형에서는 lazy사용..
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
