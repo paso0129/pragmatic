332eb6caf14cbabe02530182327f15f787d2c2b7
songjinseog <songjinseog@JhinMacBookPro.local>
2021-09-20 12:48:12 +0900

    post get 간단 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request):
-    return render(request, 'acoountapp/hello_world.html')
+
+    if request.method == "POST":
+        return render(request, 'acoountapp/hello_world.html', context={'text':'POST METHOD!!!'})# context = data꾸러미
+    else:
+        return render(request, 'acoountapp/hello_world.html', context={'text':'GET METHOD!!!'})# context = data꾸러미
